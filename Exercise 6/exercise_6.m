c70917e9bc810f213f383a15fa529fd835c39d7f
Christian Segercrantz <segercc1@aalto.fi>
2022-02-09 17:01:55 +0200

    Exercise 6

--- Exercise 6/exercise_6.m
+++ Exercise 6/exercise_6.m
@@ -13,17 +13,8 @@ x = [2,2,3];
 % Utilities of the pieces.
 u = [2,10,11];
 
-function opt_util = christian_segercrantz_dp(m,n,y,x,u)
-    if(and(all(m<x),all(n<y)))
-        opt_util = 0;
-    elseif(all(m<x))
-        
-    elseif(all(n<y))
-        
-    end
-    
 
-end
+
 
 % Calling your DP-algorithm, to solve the problem of cutting the m x n block
 % into smaller pieces. Assume that y, x, and u are ordered correctly.
@@ -32,3 +23,33 @@ end
 tic;
 opt_util = christian_segercrantz_dp(m,n,y,x,u);
 toc;
+opt_util
+function opt_util = christian_segercrantz_dp(m,n,y,x,u)
+    global sol_mat num
+    sol_mat = NaN(m,n);
+    num = length(u);
+    opt_util = my_dyn_opter(m,n,y,x,u);
+    function util = my_dyn_opter(m,n,y,x,u)
+        if(or(n==0,m==0))
+            util = 0;
+        elseif(not(isnan(sol_mat(m,n))))
+            util = sol_mat(m,n);
+        elseif(or(all(m<x),all(n<y)))
+            sol_mat(m,n) = 0;
+            util = 0;
+        else
+             opt = zeros(1,num);
+             if(and(all(m>x),all(n>y)))
+                 for i = find(and(n-x>0,m-y>0))
+                     opt(i) = max(my_dyn_opter(m,n-x(i),y,x,u) + my_dyn_opter(m,x(i),y,x,u), my_dyn_opter(m-y(i),n,y,x,u)+my_dyn_opter(y(i),n,y,x,u));
+                 end
+             else
+                 for i = find(and(n-x>=0,m-y>=0))
+                     opt(i) = u(i) + max(my_dyn_opter(m,n-x(i),y,x,u), my_dyn_opter(m-y(i),n,y,x,u));
+                 end
+             end
+         util = max(opt);
+         sol_mat(m,n) = util;  
+        end
+    end
+end
