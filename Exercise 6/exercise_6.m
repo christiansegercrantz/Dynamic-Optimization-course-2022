26f028c813d2057c6e39e43e788f1e84a89876f1
Christian Segercrantz <segercc1@aalto.fi>
2022-02-18 12:00:49 +0200

    Update exercise_6.m

--- Exercise 6/exercise_6.m
+++ Exercise 6/exercise_6.m
@@ -1,3 +1,26 @@
+% Clear all variables from memory.
+clear all;
+clc;
+
+% Dimensions of the large marble block.
+m = 100;
+n = 100;
+
+% Dimensions of the pieces.
+y = [1,4,3];
+x = [2,2,3];
+
+% Utilities of the pieces.
+u = [2,10,11];
+
+% Calling your DP-algorithm, to solve the problem of cutting the m x n block
+% into smaller pieces. Assume that y, x, and u are ordered correctly.
+% As a fun competition, we will compare your solutions based on performance
+% (hence the tic and toc statements).
+tic;
+opt_util = christian_segercrantz_dp(m,n,y,x,u);
+toc;
+opt_util
 function opt_util = christian_segercrantz_dp(m,n,y,x,u)
     %We create the global variables needed
     global sol_mat num
